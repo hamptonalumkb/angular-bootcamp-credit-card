e103f11becbec23be68a38685df2a2603b43d3d5
hamptonalumkb <124802224+hamptonalumkb@users.noreply.github.com>
2023-09-21 11:48:47 -0400

    Add name form control and validators

--- creditcard/src/app/card-form/card-form.component.ts
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-form.component.css']
 })
 export class CardFormComponent {
+
+  cardForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ] ),
+  });
+
+  constructor(){}
+
+
 
 }
